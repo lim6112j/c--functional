c7bf950c0f5507ab686faee7cec7534337cb8a68
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2022-07-20 16:14:23 +0900

    pure test with DI via constructor

--- Common/Program.cs
+++ Common/Program.cs
@@ -2,33 +2,46 @@
 using System.Text.RegularExpressions;
 namespace Common
 {
-        public abstract record Command(DateTime Timestamp);
-        public record MakeTransfer(
-            string Beneficiary,
-            string Iban,
-            string Bic,
-            DateTime Date,
-            decimal Amount,
-            string Reference,
-            DateTime Timestamp = default
-        ) : Command(Timestamp)
-        {
-            public static MakeTransfer Dummy => new(default!, default!, default!, default!, default!, default!, default!);
-        }
-        public interface IValidator<T>
-        {
-            bool IsValid(T t);
-        }
-        public class BicFormatValidator : IValidator<MakeTransfer>
-        {
-            static readonly Regex regex = new Regex("^[A-Z]{6}[A-Z1-9]{5}$");
+    public abstract record Command(DateTime Timestamp);
+    public record MakeTransfer(
+        string Beneficiary,
+        string Iban,
+        string Bic,
+        DateTime Date,
+        decimal Amount,
+        string Reference,
+        DateTime Timestamp = default
+    ) : Command(Timestamp)
+    {
+        public static MakeTransfer Dummy => new(default!, default!, default!, default!, default!, default!, default!);
+    }
+    public interface IValidator<T>
+    {
+        bool IsValid(T t);
+    }
+    public class BicFormatValidator : IValidator<MakeTransfer>
+    {
+        static readonly Regex regex = new Regex("^[A-Z]{6}[A-Z1-9]{5}$");
 
-            public bool IsValid(MakeTransfer transfer) => regex.IsMatch(transfer.Bic);
-        }
-        public class DateNotPastValidator : IValidator<MakeTransfer>
+        public bool IsValid(MakeTransfer transfer) => regex.IsMatch(transfer.Bic);
+    }
+    public interface IDateTimeService
+    {
+        DateTime UtcNow { get; }
+    }
+    public class DefaultDateTimeService : IDateTimeService
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+    public class DateNotPastValidator : IValidator<MakeTransfer>
+    {
+        private readonly IDateTimeService dataService;
+        public DateNotPastValidator(IDateTimeService dateService)
         {
-            public bool IsValid(MakeTransfer transfer) => (DateTime.UtcNow.Date <= transfer.Date.Date);
+            this.dataService = dateService;
         }
+        public bool IsValid(MakeTransfer transfer) => dataService.UtcNow <= transfer.Date.Date;
+    }
     public class Common
     {
         internal static void Main()
--- CommonTests/UnitTest1.cs
+++ CommonTests/UnitTest1.cs
@@ -3,15 +3,31 @@ namespace CommonTests;
 using Common;
 public class Tests
 {
+    static DateTime presentDate = new DateTime(2025, 3, 12);
+    private class FakeDateTimeService : IDateTimeService
+    {
+        public DateTime UtcNow => presentDate;
+    }
     [Test]
     public void WnenTransferDateIsFuture_ThenValidationPasses()
     {
-        var sut = new DateNotPastValidator();
+        // ikmpure test
+        var sut = new DateNotPastValidator(new DefaultDateTimeService());
         var transfer = MakeTransfer.Dummy with
         {
             Date = new DateTime(2025, 3, 12)
         };
         var actual = sut.IsValid(transfer);
         Assert.That(actual, Is.EqualTo(true));
+    }
+    [Test]
+    public void WhenTransferDateIsFuture_ThenValidationPassesWithFakeData()
+    {
+        var sut = new DateNotPastValidator(new FakeDateTimeService());
+        var transfer = MakeTransfer.Dummy with
+        {
+            Date = presentDate.AddDays(-1)
+        };
+        Assert.That(sut.IsValid(transfer), Is.EqualTo(false));
     }
 }
