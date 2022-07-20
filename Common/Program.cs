6569d139a3cd6f4d1ece2edfe4661063c15e72f1
lim6112j <lim6112j@gmail.com>
2022-07-21 00:57:33 +0900

    chapter 4.2.4

--- Common/Program.cs
+++ Common/Program.cs
@@ -38,11 +38,44 @@ namespace Common
     {
         public bool IsValid(MakeTransfer transfer) => Clock().Date <= transfer.Date.Date;
     }
+    enum Risk { Low, Medium, High }
+    enum Gender { Female, Male }
+    public struct Age
+    {
+        private int Value { get; }
+        public Age(int value)
+        {
+            if (!IsValid(value))
+            {
+                throw new ArgumentException($"{value} is not a valid age");
+            }
+            Value = value;
+        }
+        private static bool IsValid(int age) => 0 <= age && age < 120;
+        public static bool operator <(Age l, Age r)
+            => l.Value < r.Value;
+        public static bool operator >(Age l, Age r)
+            => l.Value > r.Value;
+        public static bool operator <(Age l, int r)
+            => l < new Age(r);
+        public static bool operator >(Age l, int r)
+            => l > new Age(r);
+    }
     public class Common
     {
+
+        static Risk CalculateRiskProfile(Age age, Gender gender)
+        {
+            if (age < 0 || age > 120)
+            {
+                throw new ArgumentException($"{age} is not a valid age");
+            }
+            return (age < 60) ? Risk.Low : Risk.High;
+        }
         internal static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Risk risk = CalculateRiskProfile(new Age(10), Gender.Male);
+            Console.WriteLine("Hello, World! {0}", risk);
 
         }
     }
