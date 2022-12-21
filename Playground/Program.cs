f8197a638c436a0afb2f8683dd404f32275c6ad8
ben lim <lim6112j@gmail.com>
2022-12-21 14:56:44 +0900

    swapargs

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -22,37 +22,44 @@ namespace Playground
             return count;
         }
         /// <summary> compose functions </summary>
-        public static Func<T, TReturn2> Compose<T, TReturn1, TReturn2>(this Func<TReturn1, TReturn2> func1, Func<T, TReturn1> func2) {
+        public static Func<T, TReturn2> Compose<T, TReturn1, TReturn2>(this Func<TReturn1, TReturn2> func1, Func<T, TReturn1> func2)
+        {
             return x => func1(func2(x));
         }
         /// <summary> string builder implementation </summary>
         public static StringBuilder AppendWhen(this StringBuilder sb, string value, bool predicate) => predicate ? sb.Append(value) : sb;
         /// <summary> generic Where custom function </summary>
-        public static IEnumerable<T> WhereCustom<T>(this IEnumerable<T> ts, Func<T, bool> predicate) {
-            foreach(T t in ts) {
-                if(predicate(t))
+        public static IEnumerable<T> WhereCustom<T>(this IEnumerable<T> ts, Func<T, bool> predicate)
+        {
+            foreach (T t in ts)
+            {
+                if (predicate(t))
                     yield return t;
             }
         }
+        /// <summary> swapargs </summary>
+        public static Func<T2, T1, R> SwapArgs<T1, T2, R>(this Func<T1, T2, R> f) => (x, y) => f(y, x);
     }
     public class Playground
     {
-        private static bool dividedBy3(int num) {
-            return num % 3 == 0;
+        private static Func<int, bool> dividedBy(int num)
+        {
+            return (n) => n % num == 0;
         }
         private static IEnumerable<int> GreaterThan(int[] a, int v)
         {
-            foreach(int n in a) {
+            foreach (int n in a)
+            {
                 if (n > v) yield return n;
             }
         }
         public static void Main()
         {
-            List<int> list = new List<int>{1,2,3, 4,5,6};
-            Func<int, bool> pred = dividedBy3;
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6 };
+            Func<int, bool> pred = dividedBy(3);
             Console.WriteLine("Linq count : " + list.Count(pred));
 
-            Func<int, int> square = x => x*x;
+            Func<int, int> square = x => x * x;
             Func<int, int> negate = x => x * -1;
             Func<int, string> toString = s => s.ToString();
             Func<int, string> squareNegateThenToString = toString.Compose(negate).Compose(square);
@@ -75,13 +82,20 @@ namespace Playground
             Console.WriteLine("added AppendWhen to StringBuilder : " + htmlButton);
 
             // yield IEnumerable
-            int [] a = { 1,2,3,4,5,6,7,8,9 };
-            foreach(int n in GreaterThan(a, 3)) {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (int n in GreaterThan(a, 3))
+            {
                 Console.WriteLine("{1,2,3,4,5} greater than 3 : " + n);
             }
-            foreach(int n in a.WhereCustom(x => x % 3 == 0)) {
+            foreach (int n in a.WhereCustom(x => x % 3 == 0))
+            {
                 Console.WriteLine("HOF Where function : " + n);
             }
+
+            // swap args
+            Func<int, int, int> divideBy = (x, y) => x / y;
+            var divideBySwapped = divideBy.SwapArgs();
+            Console.WriteLine("swapArgs func : x/y to y/x ,5/10 to  10/5 : " + divideBySwapped(5, 10));
         }
 
     }
