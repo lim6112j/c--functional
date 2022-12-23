1a68caefcfa23900fae6bd4beb058a73436efa32
lim6112j <lim6112j@gmail.com>
2022-12-23 22:59:36 +0900

    some record custom constructor

--- OptionMake/Program.cs
+++ OptionMake/Program.cs
@@ -4,7 +4,13 @@ abstract record Option<T>
     public static implicit operator Option<T>(NoneType _) => new None<T>();
 }
 record None<T> : Option<T>;
-record Some<T>(T value) : Option<T>;
+record Some<T> : Option<T> {
+    private T Value {get;}
+    public Some(T value)
+        => Value = value ?? throw new ArgumentNullException();
+    public void Deconstruct(out T value)
+        => value = Value;
+}
 static class OptionExt
 {
     public static R Match<T, R>(this Option<T> opt, Func<R> None, Func<T, R> Some) => opt switch
@@ -16,15 +22,18 @@ static class OptionExt
 }
 class OptionMake
 {
+    public static Option<T> Some<T>(T t) => new Some<T>(t);
     static readonly NoneType None = default;
     static string Greet(Option<string> greetee) => greetee.Match
    (
-       None: () => "sorry, wwho?",
-       Some: (name) => $"hello, {name}"
+       () => "sorry, wwho?",
+       (name) => $"hello, {name}"
    );
     public static void Main()
     {
         var greet = Greet(None);
         Console.WriteLine(greet);
+        var greet2 = Greet(Some("John"));
+        Console.WriteLine(greet2);
     }
 }
