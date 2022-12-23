8c651e49b79349e37700f2857ccc9f8315fe51ef
lim6112j <lim6112j@gmail.com>
2022-12-24 00:33:05 +0900

    partial function , total function

---
+++ PartialFunction/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
+using static System.Console;
+public static class Int {
+    public static Option<int> Parse(string s) => int.TryParse(s, out int result) ? Some(result) : None;
+}
+public class Partial {
+    public static void Main() {
+        var val = Int.Parse("10");
+        var val2 = Int.Parse("hello");
+        WriteLine("val : " + val);
+        WriteLine("val2 : " + val2);
+    }
+}
