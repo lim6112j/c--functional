07be5895700e14d9a7277d02d438f8a26cf9e1dd
ben lim <lim6112j@gmail.com>
2022-12-23 16:32:49 +0900

    option making

---
+++ OptionMake/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/// type Option t = None | Some t
+public interface Option<T> { }
+public record None<T> : Option<T>;
+public record Some<T>(T value) : Option<T>;
+public static class OptionExt {
+    public static R Match<T, R>(this Option<T> opt, Func<R> None, Func<T,R> Some) => opt switch {
+        None<T> => None(),
+        Some<T>(var t) => Some(t),
+        _ => throw new ArgumentException("option must be none or some")
+    };
+}
+public class OptionMake
+{
+    string Greet(Option<string> greetee) => greetee.Match
+    (
+        None: () => "sorry, wwho?",
+        Some: (name) => $"hello, {name}"
+    );
+}
