789297aed939c8a1a9dc32aa4f67ff6003218e17
lim6112j <lim6112j@gmail.com>
2022-12-25 20:37:19 +0900

    chaining continued

--- FunctionalComposition/Program.cs
+++ FunctionalComposition/Program.cs
@@ -6,6 +6,10 @@ public class Program
         var joe = new Person("Joe", "blogger");
         var email = joe.AbbreviateName().AppendDomain();
         Console.WriteLine(joe + " : " + email);
+        Circle c1 = new Circle(new Point(10, 10), 10);
+        Circle c2 = c1.Modify();
+        Console.WriteLine(c1);
+        Console.WriteLine(c2);
     }
 
 }
@@ -19,4 +23,17 @@ static class FuncComposing
         => Abbreviate(p.FirstName) + Abbreviate(p.LastName);
     internal static string AppendDomain(this string localPart)
         => $"{localPart}@manning.com";
+}
+record Circle(Point Center, double Radius);
+record Point(double X, double Y);
+static class Geometry
+{
+    internal static Circle Move(this Circle c, double x, double y)
+        => new Circle(new Point(c.Center.X + x, c.Center.Y + y), c.Radius);
+    internal static Circle Scale(this Circle c, double factor)
+        => new Circle(c.Center, c.Radius * factor);
+    internal static Circle Modify(this Circle c)
+        => c
+        .Move(10, 10)
+        .Scale(0.5);
 }
