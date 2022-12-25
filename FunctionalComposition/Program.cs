4d060bfa294bd12733bbfe7e7e156e15d6caade3
lim6112j <lim6112j@gmail.com>
2022-12-25 18:12:17 +0900

    compose function

---
+++ FunctionalComposition/Program.cs
@@ -0,0 +1,12 @@
+﻿static string Abbreviate(string s)
+    => s.Substring(0, Math.Min(2, s.Length)).ToLower();
+static string AbbreviateName(Person p)
+    => Abbreviate(p.FirstName) + Abbreviate(p.LastName);
+static string AppendDomain(string localPart)
+    => $"{localPart}@manning.com";
+Func<Person, string> emailFor =
+    p => AppendDomain(AbbreviateName(p));
+var joe = new Person("Joe", "blogger");
+var email = emailFor(joe);
+Console.WriteLine(joe + " : " + email);
+record Person(string FirstName, string LastName);
