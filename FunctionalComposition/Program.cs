1b32ee102bdc109a8212abf7fbaaa6d8a018dff6
lim6112j <lim6112j@gmail.com>
2022-12-25 19:21:26 +0900

    method chaining instead

--- FunctionalComposition/Program.cs
+++ FunctionalComposition/Program.cs
@@ -1,12 +1,22 @@
-﻿static string Abbreviate(string s)
-    => s.Substring(0, Math.Min(2, s.Length)).ToLower();
-static string AbbreviateName(Person p)
-    => Abbreviate(p.FirstName) + Abbreviate(p.LastName);
-static string AppendDomain(string localPart)
-    => $"{localPart}@manning.com";
-Func<Person, string> emailFor =
-    p => AppendDomain(AbbreviateName(p));
-var joe = new Person("Joe", "blogger");
-var email = emailFor(joe);
-Console.WriteLine(joe + " : " + email);
+﻿namespace FunctionalComposition;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var joe = new Person("Joe", "blogger");
+        var email = joe.AbbreviateName().AppendDomain();
+        Console.WriteLine(joe + " : " + email);
+    }
+
+}
+
 record Person(string FirstName, string LastName);
+static class FuncComposing
+{
+    internal static string Abbreviate(string s)
+        => s.Substring(0, Math.Min(2, s.Length)).ToLower();
+    internal static string AbbreviateName(this Person p)
+        => Abbreviate(p.FirstName) + Abbreviate(p.LastName);
+    internal static string AppendDomain(this string localPart)
+        => $"{localPart}@manning.com";
+}
