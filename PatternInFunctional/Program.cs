7b2e4fa4feb3cc4b4dfe321d32415a0b1aa1bf43
lim6112j <lim6112j@gmail.com>
2022-12-25 11:08:37 +0900

    pattern in function

---
+++ PatternInFunctional/Program.cs
@@ -0,0 +1,13 @@
+﻿//
+using static System.Linq.Enumerable;
+using LaYumba.Functional;
+var triple = (int x) => x * 3;
+Console.WriteLine(Range(1, 3).Map(triple));
+public static class PatternInFunctional
+{
+    static IEnumerable<R> Map<T, R>(this IEnumerable<T> ts, Func<T, R> f)
+    {
+        foreach (var t in ts)
+            yield return f(t);
+    }
+}
