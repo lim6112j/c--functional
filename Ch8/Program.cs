f14f744b7af249ce61577bad817770831398e878
ben lim <lim6112j@gmail.com>
2022-12-26 16:40:27 +0900

    ch8 later

---
+++ Ch8/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Ch8;
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
+public class Ch8
+{
+    // Either<L, R> = Left(L) | Right(R)
+    public static void Main()
+    {
+        var right = Right(12);
+        Console.WriteLine("Hello, World!"  + right);
+    }
+}
