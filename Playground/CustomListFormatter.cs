56d12d55630b065f86db0521b7b81566833e23ea
ben lim <lim6112j@gmail.com>
2022-12-27 16:15:46 +0900

    prettier

--- Playground/CustomListFormatter.cs
+++ Playground/CustomListFormatter.cs
@@ -1,30 +1,33 @@
 using P = System.Linq.ParallelEnumerable;
 namespace Playground;
-    class ListFormatter {
-        int counter;
-        string PrependCounter(string s) => $"{++counter}. {s}"; // impure
-        public List<string> Format(List<string> list)
-            => list
-            .Select(StringExt.ToSentenceCase) // pure
-            .Select(PrependCounter) //impure
-            .ToList();
-    }
-    class ListFormatterParallel {
-        int counter;
-        string PrependCounter(string s) => $"{++counter}. {s}";
-        public List<string> Format(List<string> list)
-            => list
-            .AsParallel()
-            .Select(StringExt.ToSentenceCase)
-            .Select(PrependCounter)
-            .ToList();
+class ListFormatter
+{
+    int counter;
+    string PrependCounter(string s) => $"{++counter}. {s}"; // impure
+    public List<string> Format(List<string> list)
+        => list
+        .Select(StringExt.ToSentenceCase) // pure
+        .Select(PrependCounter) //impure
+        .ToList();
+}
+class ListFormatterParallel
+{
+    int counter;
+    string PrependCounter(string s) => $"{++counter}. {s}";
+    public List<string> Format(List<string> list)
+        => list
+        .AsParallel()
+        .Select(StringExt.ToSentenceCase)
+        .Select(PrependCounter)
+        .ToList();
 
-    }
-    static class ListFormatterStatic {
-        public static List<string> Format(List<string> list)
-            => list
-            .AsParallel()
-            .Select(StringExt.ToSentenceCase)
-            .Zip(P.Range(1, list.Count), (s, i) => $"{i}. {s}")
-            .ToList();
-    }
+}
+static class ListFormatterStatic
+{
+    public static List<string> Format(List<string> list)
+        => list
+        .AsParallel()
+        .Select(StringExt.ToSentenceCase)
+        .Zip(P.Range(1, list.Count), (s, i) => $"{i}. {s}")
+        .ToList();
+}
