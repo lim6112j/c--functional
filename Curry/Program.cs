4c9ae67f78db9666c73b64c2b6961e6aa73b64d0
ben lim <lim6112j@gmail.com>
2023-01-25 16:40:23 +0900

    small modification

--- Curry/Program.cs
+++ Curry/Program.cs
@@ -1,13 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 namespace Curry;
 static class extension {
+  // extension func should be in non-generic static class
     public static Func<T1, Func<T2, R>> CurryFunc<T1, T2, R> (this Func<T1, T2, R> f) => t1 => t2 => f(t1, t2);
 }
 public class Program {
+  // delegate
     static Func<String, String, String> greet = (String greeting, String name) => $"{greeting}, {name}";
     private static void Main() {
-        var curried = greet.CurryFunc();
-        Console.WriteLine(greet("hello", "lim"));
-        Console.WriteLine(curried("hello")("lim"));
+        var curriedGreet = greet.CurryFunc();
+        Console.WriteLine(greet("Hello", "Ciel"));
+        Console.WriteLine(curriedGreet("Hello")("Ciel"));
     }
 }
