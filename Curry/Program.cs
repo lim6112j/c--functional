7637d57d1121f24f6fcccca046da608b09ab3f10
ben lim <lim6112j@gmail.com>
2023-01-25 16:34:24 +0900

    curried func

---
+++ Curry/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Curry;
+static class extension {
+    public static Func<T1, Func<T2, R>> CurryFunc<T1, T2, R> (this Func<T1, T2, R> f) => t1 => t2 => f(t1, t2);
+}
+public class Program {
+    static Func<String, String, String> greet = (String greeting, String name) => $"{greeting}, {name}";
+    private static void Main() {
+        var curried = greet.CurryFunc();
+        Console.WriteLine(greet("hello", "lim"));
+        Console.WriteLine(curried("hello")("lim"));
+    }
+}
