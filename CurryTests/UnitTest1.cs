9a537e1b2f89100336778ee1c9f5ac8cf6802f53
ben lim <lim6112j@gmail.com>
2023-01-30 00:04:51 +0900

    add tests

---
+++ CurryTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CurryTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ MemoizeTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MemoizeTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
