cfb4d485a52b249a3e3f66ac59eb97ea0ab68680
ben lim <lim6112j@gmail.com>
2023-01-30 00:45:45 +0900

    curry modifed

--- Curry/Program.cs
+++ Curry/Program.cs
@@ -1,21 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text;
-
 namespace Curry;
-static class extension {
+public static class Extension {
   // extension func should be in non-generic static class
     public static Func<T1, Func<T2, R>> CurryFunc<T1, T2, R> (this Func<T1, T2, R> f) => t1 => t2 => f(t1, t2);
     public static Func<T, R2> Compose<T, R1, R2>(this Func<R1, R2> f1, Func<T, R1> f2) {
       return x => f1(f2(x));
     }
     public static StringBuilder AppendWhen(this StringBuilder sb, string value, bool predicate) => predicate ? sb.Append(value) : sb;
+  // delegate
+    public static Func<String, String, String> greet = (String greeting, String name) => $"{greeting}, {name}";
 }
 public class Program {
-  // delegate
-    static Func<String, String, String> greet = (String greeting, String name) => $"{greeting}, {name}";
     private static void Main() {
-        var curriedGreet = greet.CurryFunc();
-        Console.WriteLine(greet("Hello", "Ciel"));
+        var curriedGreet = Extension.greet.CurryFunc();
+        Console.WriteLine(Extension.greet("Hello", "Ciel"));
         Console.WriteLine(curriedGreet("Hello")("Ciel"));
         // extension method : method chaining
         Func<int, int> square = (x) => x * x;
