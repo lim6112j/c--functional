d11c1579a9fe541ec30392fa7f8d8d7328201b69
ben lim <lim6112j@gmail.com>
2023-02-05 01:04:20 +0900

    formatting

--- Curry/Program.cs
+++ Curry/Program.cs
@@ -1,21 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text;
 namespace Curry;
-public static class Extension {
-  // extension func should be in non-generic static class
-    public static Func<T1, Func<T2, R>> CurryFunc<T1, T2, R> (this Func<T1, T2, R> f) => t1 => t2 => f(t1, t2);
-    public static Func<T, R2> Compose<T, R1, R2>(this Func<R1, R2> f1, Func<T, R1> f2) {
-      return x => f1(f2(x));
+public static class Extension
+{
+    // extension func should be in non-generic static class
+    public static Func<T1, Func<T2, R>> CurryFunc<T1, T2, R>(this Func<T1, T2, R> f) => t1 => t2 => f(t1, t2);
+    public static Func<T, R2> Compose<T, R1, R2>(this Func<R1, R2> f1, Func<T, R1> f2)
+    {
+        return x => f1(f2(x));
     }
     public static StringBuilder AppendWhen(this StringBuilder sb, string value, bool predicate) => predicate ? sb.Append(value) : sb;
-  // delegate
+    // delegate
     public static Func<String, String, String> greet = (String greeting, String name) => $"{greeting}, {name}";
 }
-public class Program {
-    private static void Main() {
+public class Program
+{
+    private static void Main()
+    {
         var curriedGreet = Extension.greet.CurryFunc();
         Console.WriteLine(Extension.greet("Hello", "Ciel"));
         Console.WriteLine(curriedGreet("Hello")("Ciel"));
+
         // extension method : method chaining
         Func<int, int> square = (x) => x * x;
         Func<int, int> negate = x => x + -1;
